d4d3b9ab3c59b3f916e43845190f580c862a3620
Jordan Naei <jordannaei.engineer@gmail.com>
2021-01-09 16:14:08 -0500

    added some fixes to the build

---
+++ build/precache-manifest.1ba7e156b34bee2cfe5f27fc783b7c3d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "065de52cd080db73cdf3d4472dfbed62",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac2e1eac3c51a6f8f9e2",
+    "url": "/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "e6b9c06111d18d38c9a9",
+    "url": "/static/css/main.ec8ae515.chunk.css"
+  },
+  {
+    "revision": "ac2e1eac3c51a6f8f9e2",
+    "url": "/static/js/2.e9a516d6.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.e9a516d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6b9c06111d18d38c9a9",
+    "url": "/static/js/main.d34bd53c.chunk.js"
+  },
+  {
+    "revision": "3eaec5ba2d859ccbd2c8",
+    "url": "/static/js/runtime-main.d68be137.js"
+  }
+]);
